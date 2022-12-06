04098d4d640161e8cda57bd7ac7bfa6679889a99
Abdeljalil <abdeljalil@MacBook-Pro-de-Abdeljalil.local>
2022-12-06 10:57:18 +0100

    remove duplicates script

--- lanchAssembly_megahit.sh
+++ lanchAssembly_megahit.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-#bash lanchAssembly_megahit.sh {folder}
+#SBATCH --job-name=incompl_megahit
+#SBATCH --cpus-per-task=28
+#SBATCH --mem=250GB
+#SBATCH --time=1-00:00:00
+#SBATCH --partition=fast
+#SBATCH -o /shared/projects/virome_human_apes/virome_analysis/data_incompl/slurm_hum_data_megahit.%N.%j.out
+#SBATCH -e /shared/projects/virome_human_apes/virome_analysis/data_incompl/slurm_hum_data_megahit.%N.%j.err
+#SBATCH -A virome_human_apes
 
-#Abdeljalil v1.0.0
-#SLS 20/09/2022
 
+
+
+# Useful information to print
+echo '########################################'
+echo 'Date:' $(date --iso-8601=seconds)
+echo 'User:' $USER
+echo 'Host:' $HOSTNAME
+echo 'Job Name:' $SLURM_JOB_NAME
+echo 'Job Id:' $SLURM_JOB_ID
+echo 'Directory:' $(pwd)
+echo '########################################'
+
+
+##script
+
+#source activate spades_3.15.5
 module load megahit
 
-
-folderInput=$1
+folderInput="/shared/projects/virome_human_apes/virome_analysis/data_incompl/done_incompl/"
 #refGenome="/data/virome_analysis/chimb_data/ref_genome"
 
 
@@ -18,26 +38,32 @@ echo $folder
 
 for i in ${folder};
 do
- cd ${i}
- R1fastQgz=$(ls | grep -i after_R1*\.fq.gz)
+ cd ${i}                  
+ R1fastQgz=$(ls | grep -i R1_repair_paired*\.fq.gz)
  echo $R1fastQgz
  for R1fastQgzFile in ${R1fastQgz};
  do
-  R2fastQgzFile=$(echo ${R1fastQgzFile} | sed 's/R1/R2/')
-  contigFasta=$(echo ${R1fastQgzFile} | sed 's/_after_R1.fq.gz/_assembly_megahit_out/')
+  R2fastQgzFile=$(echo ${R1fastQgzFile} | sed 's/R1/R2/') 
+  contigFasta=$(echo ${R1fastQgzFile} | sed 's/-R1_repair_paired.fq.gz/_assembly_megahit_out/')
+  prefix=$(echo ${R1fastQgzFile} |  sed 's/-R1_repair_paired.fq.gz/_out/')
   if [ -f "${R2fastQgzFile}" ];
    then
     echo "PairedEnd assembly Sample"
-    megahit -t 28 -1 ${R1fastQgzFile} -2 ${R2fastQgzFile} -o ${contigFasta}  
-    echo "assembly megahit done"
-    #cd ${contigFasta}
-     
+    srun megahit -t 28 --out-prefix ${prefix} --min-contig-len 500  -1 ${R1fastQgzFile} -2 ${R2fastQgzFile} -o ${contigFasta}
+    echo "megahit assembly done"
   else
    echo "SingleEnd Sample"
-   megahit -t 20 -s ${R1fastQgzFile} -o ${contigFasta}
-   echo "Assembly done"
+   srun megahit -t 28 --out-prefix ${prefix} --min-contig-len 500 -r ${R1fastQgzFile} -o ${contigFasta}
+   echo "Megahit Assembly done"
   fi
  cd ../
  done
 done
+#source deactivate
+
 module unload megahit
+
+
+echo '########################################'
+echo 'Job finished' $(date --iso-8601=seconds)
+
